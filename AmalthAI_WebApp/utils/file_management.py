4fed9435b533dcef31328c12fa3270c463a17ac8
cchatzisa <chrichatzisavvas@gmail.com>
2026-06-22 10:52:23 +0000

    Removes unnecessary files

--- AmalthAI_WebApp/utils/file_management.py
+++
@@ -1,7 +0,0 @@
-import os 
-import shutil
-
-# a tiny function to remove intermediate files
-def remove_unnecessary_files(path):
-    if os.path.exists(path) and os.path.isdir(path):
-        shutil.rmtree(path)
